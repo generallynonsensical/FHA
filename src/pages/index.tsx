8f54a5055f1826cb4cc67d5ba15b5921d64e443c
generallynonsensical <winstoncroberts@protonmail.com>
2023-11-13 07:45:58 -0700

    ..

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,19 +2,19 @@
 
 import React from 'react';
 import AccordionContainer from '../containers/AccordionContainer';
-import Tables from '../components/Tables';
+import Tables from '../containers/TableContainer';
 import PDFGenerator from '../components/PDFGenerator';
 import UndoRedoButtons from '../components/UndoRedoButtons';
 
 const App: React.FC = () => {
   return (
     <div className="flex">
-      <div className="p-2 border-r border-gray-300 w-2/10">
+      <div className="p-2 border-r border-gray-300">
         <AccordionContainer />
         <UndoRedoButtons onUndo={() => {}} onRedo={() => {}} />
       </div>
 
-      <div className="p-2 border-l border-gray-300 w-7/10">
+      <div className="p-2 border-l border-gray-300">
         <Tables />
         <PDFGenerator onSave={() => {}} />
       </div>
